ba78eb494bfb6eedafda7816d84ce692dbc923d4
Samuel Pampolini <samuelpampolini@gmail.com>
2019-06-14 15:54:11 -0300

    Projeto de gerenciador de tarefas

---
+++ calculadora/src/app/calculadora/services/calculadora.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculadoraService {
+  /*Define as contastantes utilizadas
+  para identificar as operações de calculo*/
+  static readonly SOMA: string = "+";
+  static readonly SUBTRACAO: string = "-";
+  static readonly DIVISAO: string = "/";
+  static readonly MULTIPLICACAO: string = "*";
+
+  constructor() { }
+
+  /**
+   * Método para calculos das operações básicas.
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string operação a ser executada.
+   * @return number Resultado da operação
+  */
+  calcular(num1: number, num2: number, operacao: string) :number {
+    let resultado: number;
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+          resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+          resultado = num1 - num2;
+        break;
+      case CalculadoraService.DIVISAO:
+          resultado = num1 / num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+          resultado = num1 * num2;
+        break;
+      default:
+          resultado = 0;
+    }
+
+    return resultado;
+  }
+}
